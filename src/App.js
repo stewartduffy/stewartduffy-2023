c3841cee83aca461bf31a7894a9c66e7fd162916
Stewart Duffy <duffy.stewart@gmail.com>
2023-11-04 22:59:00 +1300

    Migrate old website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,170 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      jobs: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = [
+      // Length issue
+      `https://gist.githubusercontent.com`,
+      `/stewartduffy/1b0a9759dfba523807514af7d11df6ca`,
+      `/raw/stewartduffy-data.json`,
+    ].join("");
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({
+          jobs: data.jobs,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="header">
+          <h1 className="heading-logo text-hide">
+            Stewart Duffy / Front-End Developer..
+          </h1>
+          <div className="nav-container clearfix">
+            <div className="container">
+              <nav className="pull-left hidden-xs">
+                <a
+                  className="nav-icon"
+                  href="http://nz.linkedin.com/in/stewartduffy/en"
+                >
+                  <i className="fa fa-linkedin fa-fw" />
+                </a>
+                <a className="nav-icon" href="http://github.com/stewartduffy">
+                  <i className="fa fa-github-alt fa-fw" />
+                </a>
+                <a
+                  className="nav-icon"
+                  href="https://open.spotify.com/user/1143680138"
+                >
+                  <i className="fa fa-spotify fa-fw" />
+                </a>
+              </nav>
+              <nav className="pull-right hidden-xs">
+                <a className="nav-item" href="#contact">
+                  contact
+                </a>
+              </nav>
+            </div>
+          </div>
+        </div>
+
+        <div className="container main-container">
+          <div className="main">
+            <div className="row">
+              <div className="col-md-1 hidden-sm hidden-xs">
+                <i className="main-icon fa fa-anchor" />
+              </div>
+              <div className="col-md-11">
+                <p>
+                  Hello! I'm Stewart Duffy, a{" "}
+                  <strong>Front-End Developer</strong> who makes websites and
+                  apps. I mostly trade in the trifecta of HTML5, CSS3 &amp;
+                  JavaScript, but like any self-respecting digital worker I
+                  utilise an array of other tools &amp; tech to get the job
+                  done..
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-1 hidden-sm hidden-xs">
+                <i className="main-icon fa fa-coffee" />
+              </div>
+              <div className="col-md-11">
+                <p>
+                  I enjoy what I do, and I love the web development community.
+                  Naturally I like to meet other fellow web workers, so I
+                  regularly go to local meetups, and I jump at the opportunity
+                  to attend conferences &amp; workshops to learn from the
+                  masters.
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-1 hidden-sm hidden-xs">
+                <i className="main-icon fa fa-code" />
+              </div>
+              <div className="col-md-11">
+                <h3>Conferences &amp; Workshops</h3>
+                <ul className="list-unstyled">
+                  <li>Codemania 2016 / Auckland, New Zealand</li>
+                  <li>JSConf EU 2013 / Berlin, Germany</li>
+                  <li>Full Frontal Tooling Tutorials 2012 / Brighton, UK</li>
+                  <li>dotJS 2012 / Paris, France</li>
+                  <li className="last">WDCNZ 2011 / Wellington, New Zealand</li>
+                </ul>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-1 hidden-sm hidden-xs">
+                <i className="main-icon fa fa-bullhorn" />
+              </div>
+              <div className="col-md-11">
+                <p>
+                  I have been working in the industry for over six years;
+                  working on a variety of projects either through freelancing,
+                  contracting or permanent employment. With agency, startup
+                  &amp; client-side experience I have worked on projects in
+                  multiple industries including Education, Retail,
+                  Telecommunications, Print Media, Government and Advertising.
+                </p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-1 hidden-sm hidden-xs">
+                <i className="main-icon fa fa-wrench" />
+              </div>
+
+              <div id="cta-work-history" className="col-md-11">
+                <h3>Work History</h3>
+                <ul className="list-unstyled">
+                  {this.state.jobs.map((job, index) => (
+                    <li>
+                      <a href={job.link}>{job.name}</a> / {job.city}
+                      <br />
+                      <small>
+                        {job.startDate} – {job.endDate}
+                      </small>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-1 hidden-sm hidden-xs">
+                <i className="main-icon fa fa-phone" />
+              </div>
+              <div id="cta-contact" className="col-md-11">
+                <h3 id="contact">Let’s talk</h3>
+                <p>
+                  I am not currently looking for something new, however I am
+                  always happy to talk. If you did want to chat you can message
+                  me on{" "}
+                  <a href="http://nz.linkedin.com/in/stewartduffy/en">
+                    LinkedIn
+                  </a>{" "}
+                  .
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
